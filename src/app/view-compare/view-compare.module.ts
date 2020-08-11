f44e08994a304f8a97bf8d47f6383bada8d93ed4
rafiqahzara <69479910+rafiqahzara@users.noreply.github.com>
2020-08-11 17:40:20 +0800

    Add files via upload

--- src/app/view-compare/view-compare.module.ts
+++ src/app/view-compare/view-compare.module.ts
@@ -1,1 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { IonicModule } from '@ionic/angular';
+
+import { ViewComparePageRoutingModule } from './view-compare-routing.module';
+
+import { ViewComparePage } from './view-compare.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ViewComparePageRoutingModule
+  ],
+  declarations: [ViewComparePage]
+})
+export class ViewComparePageModule {}
