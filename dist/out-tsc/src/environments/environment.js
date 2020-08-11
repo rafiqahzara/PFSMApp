d7427707e8b8234b4538589be4ece39f49337505
rafiqahzara <69479910+rafiqahzara@users.noreply.github.com>
2020-08-11 23:43:45 +0800

    Create environment.js

---
+++ dist/out-tsc/src/environments/environment.js
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDcbVmXC-37RxzS0NpgdHmrkZuVMcsQYSE",
+        authDomain: "pro-skin.firebaseapp.com",
+        databaseURL: "https://pro-skin.firebaseio.com",
+        projectId: "pro-skin",
+        storageBucket: "pro-skin.appspot.com",
+        messagingSenderId: "298383182120",
+        appId: "1:298383182120:web:0232c9851e1a16439f681d",
+        measurementId: "G-VB38P0FG2F"
+    }
+};
+//# sourceMappingURL=environment.js.map
