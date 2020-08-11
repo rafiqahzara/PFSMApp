c0d992c0d92aa5dc009cd1178d186e24f3bcb895
rafiqahzara <69479910+rafiqahzara@users.noreply.github.com>
2020-08-11 17:36:07 +0800

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+import { AuthenticationService } from './services/authentication.service';
+
+
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { HttpClientModule } from "@angular/common/http";
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+import * as firebase from 'firebase';
+
+firebase.initializeApp(environment.firebase);
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireAuthModule,
+
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    
+    HttpClientModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+    ],
+
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AuthenticationService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
